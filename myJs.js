7660f147c2b6dfea53cd6d3e8b72034a7c6b988f
Khánh <93875222+khanhnvph16474@users.noreply.github.com>
2023-09-19 10:19:33 +0700

    Update myJs.js

--- myJs.js
+++ myJs.js
@@ -138,7 +138,7 @@ $(document).ready(function () {
           text: textConfig.text11,
           confirmButtonColor: "#83d0c9",
           onClose: () => {
-            window.location = "https://www.facebook.com/Khactruong38/";
+            window.location = "https://www.facebook.com/quangnezZz2002";
           },
         });
       }
